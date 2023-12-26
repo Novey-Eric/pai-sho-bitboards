f5186ed2bc6cc32b9826755eb41bc2442dd34401
eric <novey.eric@gmail.com>
2023-12-26 17:00:55 -0500

    undoing move in minimax

--- src/Computer.cpp
+++ src/Computer.cpp
@@ -1,46 +1,57 @@
 #include"Computer.h"
+#include <cstring>
+#include <iostream>
 
 
 namespace Paisho{
 
 int ply;
 
-    Move minimax(Board b, int depth, int player, Move *best_move){
+    Move minimax(Board *b, int depth, int player, Move *best_move){
         //int p_mult = player ? -1 : 1; //WHITEs enum value is 0
 
-        if (depth == 0 || Bitboards::check_win(&b) != -1){
-            return evaluate(&b);
+        if (depth == 0 || Bitboards::check_win(b) != -1){
+            return evaluate(b);
         }
 
+        Board b_copy;
+        memcpy(&b_copy, b, sizeof(Board));
         int value;
         Moves curr_moves;
         Move out_move;
         if (player == WHITE){
             value = -999999;
-            curr_moves = Bitboards::get_moves(b, WHITE);
+            curr_moves = Bitboards::get_moves(b_copy, WHITE);
             for(int i = 0; i < curr_moves.move_count; i++){
                 ply++;
-                Bitboards::make_move(&b, player, curr_moves.movelist[i]);
-                int t_val = minimax(b, depth-1, BLACK, &out_move);
+                Bitboards::pretty(b_copy.otherBoards[AllPieces]);
+                std::cout<< curr_moves.movelist[i] << " ";
+                print_move(curr_moves.movelist[i]);
+                Bitboards::make_move(&b_copy, player, curr_moves.movelist[i]);
+                int t_val = minimax(&b_copy, depth-1, BLACK, &out_move);
                 ply--;
                 if (t_val > value){
                     value = t_val;
                     *best_move = curr_moves.movelist[i];
                 }
+                memcpy(&b_copy, b, sizeof(Board));
+                
             }
             return value;
         }else{
             value = 999999;
-            curr_moves = Bitboards::get_moves(b, BLACK);
+            curr_moves = Bitboards::get_moves(b_copy, BLACK);
             for(int i = 0; i < curr_moves.move_count; i++){
                 ply++;
-                Bitboards::make_move(&b, player, curr_moves.movelist[i]);
-                int t_val = minimax(b, depth-1, WHITE, &out_move);
+                print_move(curr_moves.movelist[i]);
+                Bitboards::make_move(&b_copy, player, curr_moves.movelist[i]);
+                int t_val = minimax(&b_copy, depth-1, WHITE, &out_move);
                 ply--;
                 if (t_val < value){
                     value = t_val;
                     *best_move = curr_moves.movelist[i];
                 }
+                memcpy(&b_copy, b, sizeof(Board));
             }
             return value;
         }
